446bb64094b3d3359c64e8ae26da032c66485605
sanit7722 <sanit7722@gmail.com>
2020-06-05 15:11:37 +0530

    Recyclerview with room added

--- app/src/main/java/com/sanit/todo/Adapter/ToDoAdapter.kt
+++ app/src/main/java/com/sanit/todo/Adapter/ToDoAdapter.kt
@@ -2,15 +2,18 @@ package com.sanit.todo.Adapter
 
 import android.content.Context
 import android.icu.text.Transliterator
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Adapter
 import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
-import com.sanit.todo.R
-import com.sanit.todo.ToDoDAO
-import com.sanit.todo.ToDoStore
+import com.sanit.todo.*
+import io.reactivex.Observable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
+import kotlinx.android.synthetic.main.activity_to_do.*
 import kotlinx.android.synthetic.main.item_does.view.*
 
 class ToDoAdapter(val items: List<ToDoStore>, val context: Context) :
@@ -31,12 +34,32 @@ class ToDoAdapter(val items: List<ToDoStore>, val context: Context) :
         holder.tododesc.text = items.get(position).tododesc
         holder.tododate.text = items.get(position).tododate
         holder.id = items.get(position).id
-        var todoStore: ToDoStore
+
         holder.imgdelete.setOnClickListener {
-            var id: Long?
+            val id: Long?
             id = items.get(position).id
             Toast.makeText(context, items.get(position).id.toString(), Toast.LENGTH_LONG).show()
 
+            var todoDAO: ToDoDAO? = null
+            var db: ToDoDB? = null
+            Observable.fromCallable {
+                db = ToDoDB.getAPPDataBase(context)
+                todoDAO = db?.ToDoDAO()
+
+                with(todoDAO) {
+                    this?.deleteByUserId(id)
+                }
+                notifyItemRemoved(position)
+                db?.ToDoDAO()?.getToDoList()
+            }.subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread()).subscribe(
+                {
+                    Log.d("size", it?.size.toString())
+                    notifyItemRemoved(position)
+                },
+                {
+                    Log.d("error", "error")
+                }
+            )
 
         }
 
